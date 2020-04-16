51856db48c0c51747f9f8bf8e57e8d287b87c21c
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-04-16 13:16:10 +0900

    Initialize export-ignore

---
+++ src/network/mcpe/protocol/ClientCacheMissResponsePacket.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol;
+
+#include <rules/DataPacket.h>
+
+use pocketmine\network\mcpe\handler\PacketHandler;
+use pocketmine\network\mcpe\protocol\types\ChunkCacheBlob;
+use pocketmine\network\mcpe\serializer\NetworkBinaryStream;
+use function count;
+
+class ClientCacheMissResponsePacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::CLIENT_CACHE_MISS_RESPONSE_PACKET;
+
+	/** @var ChunkCacheBlob[] */
+	private $blobs = [];
+
+	/**
+	 * @param ChunkCacheBlob[] $blobs
+	 */
+	public static function create(array $blobs) : self{
+		//type check
+		(static function(ChunkCacheBlob ...$blobs) : void{})(...$blobs);
+
+		$result = new self;
+		$result->blobs = $blobs;
+		return $result;
+	}
+
+	/**
+	 * @return ChunkCacheBlob[]
+	 */
+	public function getBlobs() : array{
+		return $this->blobs;
+	}
+
+	protected function decodePayload(NetworkBinaryStream $in) : void{
+		for($i = 0, $count = $in->getUnsignedVarInt(); $i < $count; ++$i){
+			$hash = $in->getLLong();
+			$payload = $in->getString();
+			$this->blobs[] = new ChunkCacheBlob($hash, $payload);
+		}
+	}
+
+	protected function encodePayload(NetworkBinaryStream $out) : void{
+		$out->putUnsignedVarInt(count($this->blobs));
+		foreach($this->blobs as $blob){
+			$out->putLLong($blob->getHash());
+			$out->putString($blob->getPayload());
+		}
+	}
+
+	public function handle(PacketHandler $handler) : bool{
+		return $handler->handleClientCacheMissResponse($this);
+	}
+}
